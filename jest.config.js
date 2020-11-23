1a9bb0487a9c9169a01e7d5912531677fdf32854
William Weiss <william@codification.org>
2020-11-22 21:45:33 -0800

    refactor: remove jest related functionality

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "testMatch": [
-    "**/test/**/*-jest.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-}
